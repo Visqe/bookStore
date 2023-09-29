9eedcdc63e72a231376ae159b4542415ed370247
Visqe <veetee@tutanota.com>
2023-09-29 17:05:43 +0300

    added GlobalExceptionHandler for EntityNotFoundException

--- src/main/java/mate/academy/bookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -13,11 +13,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHander extends ResponseEntityExceptionHandler {
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
@@ -25,14 +26,30 @@ public class CustomGlobalExceptionHander extends ResponseEntityExceptionHandler 
             @NotNull HttpStatusCode status,
             @NotNull WebRequest request
     ) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
+
         List<Object> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(this::getErrorMessage)
                 .toList();
-        body.put("errors", errors);
+
+        Map<String, Object> body = method(HttpStatus.BAD_REQUEST, errors);
         return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFound(
+            EntityNotFoundException exception
+    ) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        Map<String, Object> body = method(status, List.of(exception.getMessage()));
+        return new ResponseEntity<>(body, status);
+    }
+
+    private Map<String, Object> method(HttpStatus httpStatus, List<Object> errors) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", httpStatus);
+        body.put("errors", errors);
+        return body;
     }
 
     private Object getErrorMessage(ObjectError e) {
