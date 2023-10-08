4fead20fb77d0a70a29a5321bb79cbcb3472e9eb
Visqe <135153077+Visqe@users.noreply.github.com>
2023-10-08 21:31:33 +0300

    Revert "Security (#6)" (#9)
    This reverts commit 32a16ef1afc77d54a9d1c82e1f6a302b2723778e.

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,4 +1,4 @@
-package mate.academy.bookstore.dto.book;
+package mate.academy.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
--- src/main/java/mate/academy/bookstore/exception/RegistrationExeption.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.bookstore.exception;
-
-public class RegistrationExeption extends Exception {
-    public RegistrationExeption(String message) {
-        super(message);
-    }
-}
