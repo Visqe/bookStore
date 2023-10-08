4b991dcc0ad2d75575fe711b763a40d2e971e67e
Visqe <veetee@tutanota.com>
2023-10-08 21:18:52 +0300

    added isbn validation

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
@@ -13,6 +14,7 @@ public class CreateBookRequestDto {
     private String title;
     @NotBlank
     private String author;
+    @ISBN
     @NotNull
     private String isbn;
     @NotNull
